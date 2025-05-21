c93c32897a921a51487222d4de6c70e61d79657a
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-05-21 11:38:29 +0700

    Configure database with database service and database adaptor

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environment.get('DB_HOST'),
+        'NAME': os.environment.get('DB_NAME'),
+        'USER': os.environment.get('DB_USER'),
+        'PASSWORD': os.environment.get('DB_PASSWORD'),
     }
 }
 
