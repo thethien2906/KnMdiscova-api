4b07b0cdb4816abd02b34b5673f4f3c03eb6860b
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-06-04 14:27:13 +0700

    app restarting error

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from core.views import health_check
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
-
+    path('health/', health_check, name='health-check'),
     # users
     path('api/', include('users.urls')),
     # parents
