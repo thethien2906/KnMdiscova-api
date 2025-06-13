df1a49174103469026081b84aad644b3193ff6ca
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-06-13 14:43:49 +0700

    update checks

---
+++ app/core/management/commands/waif_for_redis.py
@@ -0,0 +1,29 @@
+# app/core/management/commands/wait_for_redis.py
+import time
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from redis.exceptions import ConnectionError
+import redis
+
+class Command(BaseCommand):
+    """Django command to wait for Redis to be available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for Redis...')
+        r = redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT
+        )
+        retries = 30
+        while retries > 0:
+            try:
+                r.ping()
+                self.stdout.write(self.style.SUCCESS('Redis is available!'))
+                return
+            except ConnectionError:
+                self.stdout.write(f'Redis unavailable, waiting 2 seconds... ({31 - retries}/30)')
+                retries -= 1
+                time.sleep(2)
+
+        self.stdout.write(self.style.ERROR('Could not connect to Redis!'))
+        exit(1) # Exit with a non-zero status code to fail the CI job
