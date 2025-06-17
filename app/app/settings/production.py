c9c78734d67327309625b0cf490e0c2867e646f0
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-06-17 13:03:10 +0700

    add new cors entries

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -66,6 +66,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8081',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 # Add custom trusted origins from environment
@@ -88,6 +90,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8081',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 # Add custom CORS origins from environment
